5577319220d27bc66b2b5b02512a63a7cea7530e
Christopher Sasias <102446378+ChrisSasias@users.noreply.github.com>
2023-07-02 14:37:11 -0300

    Create Planets.jsx

---
+++ src/js/component/Planets.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export const Planet = (props) => {
+	return (
+        <>
+            <div className="card m-2" style={{width: "18rem"}}>
+                <img src={"https://starwars-visualguide.com/assets/img/planets/"+props.idp+".jpg"} className="card-img-top" alt="..."/>
+            <div className="card-body">
+                <h5 className="card-title">{props.name}</h5>
+                <p className="card-text">
+                    Population: {props.population} <br/>
+                    Terrain: {props.terrain}
+                </p>
+                <Link to={"/planet/" + props.idp} className="btn btn-primary">Learn more</Link>
+            </div>
+            </div>
+        </>
+    )
+    };
+
+    export default Planet;
