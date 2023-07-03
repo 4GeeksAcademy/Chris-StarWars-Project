13a203ed450673bd3109f3187981494de0f6db44
Christopher Sasias <102446378+ChrisSasias@users.noreply.github.com>
2023-07-02 22:35:00 -0300

    Update layout.js

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
+import "../styles/home.css"
 
-import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
+import  Home  from "./views/Home.jsx";
+import PeopleDetails from "./views/PersonDetails.jsx";
+import PlanetDetails from "./views/PlanetDetails.jsx";
+import VehicleDetails from "./views/VehiclesDetails.jsx";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 
-//create your first component
+
 const Layout = () => {
-	//the basename is used when your project is published in a subdirectory and not in the root of the domain
-	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+	
 	const basename = process.env.BASENAME || "";
 
 	return (
@@ -23,8 +24,11 @@ const Layout = () => {
 					<Navbar />
 					<Routes>
 						<Route path="/" element={<Home />} />
-						<Route path="/demo" element={<Demo />} />
-						<Route path="/single/:theid" element={<Single />} />
+						<Route path="/home" element={<Home />} />
+						<Route path="/people-details" element={<PeopleDetails />} />
+						<Route path="/planet-details" element={<PlanetDetails />} />
+						<Route path="/starships-details" element={<VehicleDetails />} />
+
 						<Route path="*" element={<h1>Not found!</h1>} />
 					</Routes>
 					<Footer />
