4f15c0dfbf017044ef42bcfdbc3c397ed1208401
Christopher Sasias <102446378+ChrisSasias@users.noreply.github.com>
2023-07-02 22:21:31 -0300

    Update VehiclesDetail.jsx

--- src/js/views/VehiclesDetail.jsx
+++ src/js/views/VehiclesDetail.jsx
@@ -1,27 +1,72 @@
-import React, { useContext } from "react";
-import { Link, useParams } from "react-router-dom";
-import { Context } from "../store/appContext";
+import React from 'react';
+import { Context } from '../store/appContext.js';
+import { useContext, useState } from 'react';
 
- const Selected = (props) => {
-	const { store, actions } = useContext(Context);
-	const params = useParams();
-	return (
-		<div className="jumbotron">
-			<h1 className="display-4">This will show the demo element: {store.demo[params.theid].title}</h1>
+import {  useNavigate } from 'react-router-dom';
 
-			<hr className="my-4" />
 
-			<Link to="/">
-				<span className="btn btn-primary btn-lg" href="#" role="button">
-					Back home
-				</span>
-			</Link>
-		</div>
-	);
-};
+import "../../styles/home.css";
 
-// Single.propTypes = {
-// 	match: PropTypes.object
-// };
 
-export default Selected;
+
+
+const Vehicles = ({vehicle}) => {
+  const { store, actions } = useContext(Context);
+const navigate = useNavigate()
+
+
+
+	const [imageSource, setImageSource] = useState(`https://starwars-visualguide.com/assets/img/starships/${vehicle.result.uid}.jpg`);
+ 
+
+    
+	function handleImageError() {
+		// Código para manejar el error de carga de la imagen
+		setImageSource("https://c4.wallpaperflare.com/wallpaper/670/495/775/tv-show-the-mandalorian-baby-yoda-star-wars-the-mandalorian-tv-show-hd-wallpaper-preview.jpg");
+	  }
+
+
+     
+           
+    const handleDetails = (id) => {
+      actions.detailVehicle(id);
+      navigate("/starships-details")
+      };
+      
+    
+    
+    return(
+
+      
+	<div className="text-center mt-5">
+    
+       
+        <div className="card  m-2"  
+       style={{width:"18rem"}} >
+        <img src={imageSource} onError={handleImageError} className="card-img-top" style={{height:"18rem",objectFit:"cover",borderRadius:"20px"}} alt="..."/>
+        <div className="card-body" style={{height:"16rem",overflow:"scroll"}}>
+        <h5 className="card-title">{vehicle.result.properties.name}</h5>
+   
+            <div>
+              <p className="card-text"><span>Model:</span>{vehicle.result.properties.model}</p>
+              <p className="card-text"><span>Manufacturer:</span>{vehicle.result.properties.manufacturer} </p>
+              <p className="card-text"><span>Passengers:</span>{vehicle.result.properties.passengers} </p>
+            </div>
+        
+
+            <button onClick={() => handleDetails(vehicle.result.uid)} className="btn btn-warning m-3">Details</button>
+
+
+    <button href="#" className="btn btn-warning m-3 " onClick={() => {
+									actions.setFavoritesVehicles(vehicle)
+								}}  ><strong>♥</strong></button>
+  </div>
+</div>
+
+
+
+	
+	</div>
+);};
+
+export default Vehicles;
