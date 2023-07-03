ac8365db0b2a854a566844d1d5a4c407ec58a76a
Christopher Sasias <102446378+ChrisSasias@users.noreply.github.com>
2023-07-03 20:53:56 -0300

    Update Planets.jsx

--- src/js/component/Planets.jsx
+++ src/js/component/Planets.jsx
@@ -41,7 +41,7 @@ const navigate = useNavigate()
        
         <div className="card  m-2"  
        style={{width:"18rem"}}>
-        <img src={imageSource} onError={handleImageError} className="card-img-top" style={{height:"18rem",objectFit:"cover",borderRadius:"20px"}} alt="..."/>
+        <img src={imageSource} onError={handleImageError} className="card-img-top" style={{height:"18rem",objectFit:"cover",borderRadius:"5px"}} alt="..."/>
         <div className="card-body" style={{height:"16rem",overflow:"scroll"}}>
         <h5 className="card-title"> {planet.result.properties.name}</h5>
        
