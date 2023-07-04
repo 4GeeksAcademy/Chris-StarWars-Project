32bba9e97222c41bf1f46f5fe594243721bbded8
Christopher Sasias <102446378+ChrisSasias@users.noreply.github.com>
2023-07-04 03:12:36 +0000

    New corrections

--- src/js/views/VehiclesDetails.jsx
+++ src/js/views/VehiclesDetails.jsx
@@ -1,72 +1,72 @@
-import React from 'react';
-import { Context } from '../store/appContext.js';
-import { useContext, useState } from 'react';
+import React, { useContext, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { Context } from "../store/appContext";
 
-import {  useNavigate } from 'react-router-dom';
+export const VehicleDetails = () => {
+	
+	const { actions, store } = useContext(Context)
 
-
-import "../../styles/home.css";
-
-
-
-
-const Vehicles = ({vehicle}) => {
-  const { store, actions } = useContext(Context);
-const navigate = useNavigate()
+	
+	const [imageSource, setImageSource] = useState(`https://starwars-visualguide.com/assets/img/starships/${store.oneVehicle.result.uid}.jpg`);
 
 
-
-	const [imageSource, setImageSource] = useState(`https://starwars-visualguide.com/assets/img/starships/${vehicle.result.uid}.jpg`);
- 
-
-    
 	function handleImageError() {
 		// Código para manejar el error de carga de la imagen
 		setImageSource("https://c4.wallpaperflare.com/wallpaper/670/495/775/tv-show-the-mandalorian-baby-yoda-star-wars-the-mandalorian-tv-show-hd-wallpaper-preview.jpg");
 	  }
-
-
-     
-           
-    const handleDetails = (id) => {
-      actions.detailVehicle(id);
-      navigate("/starships-details")
-      };
-      
     
-    
-    return(
-
-      
-	<div className="text-center mt-5">
-    
-       
-        <div className="card  m-2"  
-       style={{width:"18rem"}} >
-        <img src={imageSource} onError={handleImageError} className="card-img-top" style={{height:"18rem",objectFit:"cover",borderRadius:"20px"}} alt="..."/>
-        <div className="card-body" style={{height:"16rem",overflow:"scroll"}}>
-        <h5 className="card-title">{vehicle.result.properties.name}</h5>
-   
-            <div>
-              <p className="card-text"><span>Model:</span>{vehicle.result.properties.model}</p>
-              <p className="card-text"><span>Manufacturer:</span>{vehicle.result.properties.manufacturer} </p>
-              <p className="card-text"><span>Passengers:</span>{vehicle.result.properties.passengers} </p>
-            </div>
-        
-
-            <button onClick={() => handleDetails(vehicle.result.uid)} className="btn btn-warning m-3">Details</button>
 
 
-    <button href="#" className="btn btn-warning m-3 " onClick={() => {
-									actions.setFavoritesVehicles(vehicle)
-								}}  ><strong>♥</strong></button>
-  </div>
-</div>
+	return (
+
+		<div className="text-center mx-auto ">
+
+
+			<h1>StarShip Details</h1>
+
+			{JSON.stringify(store.oneVehicle) !== '{}' ?(
+			<div className="card text-start mx-auto"
+				style={{ width: "60%" }} key="index">
+				<img src={imageSource} onError={handleImageError} className="card-img-top mx-auto mt-2" style={{ height: "18rem", objectFit: "cover", borderRadius: "20px",width: "65%" }} alt="..." />
+				<div className="card-body mx-auto" style={{ height: "16rem", overflow: "scroll",width: "80%" }}>
+					<h5 className="card-title">Name: {store.oneVehicle.result.properties.name} </h5>
+
+					<div className="row">
+						<div className="col-12 mx-auto">
+							<h5 className="row ">Description:</h5>
+							<p className="col "><i color="orange" className='bx-pull-left bx bx-tada-hover bxs-quote-alt-left bx-lg' /> Estas son 10 de las naves y vehículos mejor diseñados de la franquicia galáctica más popular de todos los tiempos.</p>
+							<div className="row">
+								<h5 className="col ">Model:</h5>
+								<p className="col ">{store.oneVehicle.result.properties.model}</p>
+								<h5 className="col ">Manufacturer:</h5>
+								<p className="col ">{store.oneVehicle.result.properties.manufacturer}</p>
+								<h5 className="col ">Passengers:</h5>
+								<p className="col ">{store.oneVehicle.result.properties.passengers}</p>
+								<h5 className="col ">Max speed:</h5>
+								<p className="col ">{store.oneVehicle.result.properties.max_atmosphering_speed}</p>
+								<h5 className="col ">Class:</h5>
+								<p className="col ">{store.oneVehicle.result.properties.starship_class}</p>
+							</div>
+						</div>
 
 
 
-	
-	</div>
-);};
+					</div>
+				</div>
 
-export default Vehicles;
+
+
+
+				<Link to="/home" className="btn btn-warning m-3">Back to Home</Link>
+
+			</div>
+			):(
+				<h1>No only one selected</h1>
+			)}
+		</div>
+	)
+
+
+};
+
+export default VehicleDetails;
