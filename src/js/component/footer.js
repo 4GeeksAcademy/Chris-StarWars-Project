9a1d87faa95213ddaa737d7b1fd6aa00649cdbcd
Christopher Sasias <102446378+ChrisSasias@users.noreply.github.com>
2023-07-04 04:17:01 +0000

    New corrections 4

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<h5>
-			Star Wars Vlog Chris
+			Star Wars Vlog 
 		</h5>
 	</footer>
 );
